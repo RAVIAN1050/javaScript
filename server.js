0c0787175b4b7caa36b04fd934dc3f74f41bab18
MUHAMMAD UMAR <umarfarhan@proton.me>
2025-06-29 05:54:06 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
 const express = require('express')
 const app = express()
 
-
-app.get('/',(req,res)=>{
-    res.json({mssg: 'Welcome to the Newly created Express Server app'})
-})
-
-
-app.listen(4000,()=>{
-    console.log("Listening on port 4000 !!!")
-})
